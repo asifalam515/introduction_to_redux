05a5522aa3bde725c4ed5b4d60309be9644a31c4
asifalam515 <asibulalam515@gmail.com>
2025-01-13 17:10:56 +0600

    store configureed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/counterSlice";
+
 const App = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <h1 className="text-5xl">Redux with Redux</h1>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-3 py-2 rounder-md bg-green-500 text-xl font-semibold text-white"
+        >
+          Increment
+        </button>
+        <h1 className="text-3xl m-10"> {count} </h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-3 py-2 rounder-md bg-red-500 text-xl font-semibold text-white"
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
